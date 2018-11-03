0a1111d9a2ceece1629e4418ba95ac6805366d9d
Oleg <super.oleg.3000@yandex.ru>
2018-11-03 20:05:43 +0300

    Write html articles to the output directory

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+import os
 
 import markdown2
-from jinja2 import FileSystemLoader, Environment, select_autoescape
+from jinja2 import FileSystemLoader, Environment
 
 
 def load_text_from_file(filepath):
@@ -13,15 +14,20 @@ def load_text_from_file(filepath):
 if __name__ == "__main__":
     json_config = json.loads(load_text_from_file("config.json"))
     articles = json_config["articles"]
-    md_path = "articles/{}".format(articles[0]["source"])
-    article_name = articles[0]["title"]
-    md_content = load_text_from_file(md_path)
-    html_content = markdown2.markdown(md_content)
-    # print(html_content)
 
-    environment = Environment(
-        loader=FileSystemLoader("templates")
-    )
-    template = environment.get_template("article.html")
-    result = template.render(name=article_name, content=html_content)
-    print(result)
+    for article in articles:
+        md_path = "articles/{}".format(article["source"])
+        article_name = article["title"]
+        md_content = load_text_from_file(md_path)
+        html_content = markdown2.markdown(md_content)
+
+        environment = Environment(loader=FileSystemLoader("templates"))
+        template = environment.get_template("article.html")
+        template_stream = template.stream(
+            name=article_name, content=html_content
+        )
+        filename, _ = os.path.splitext(os.path.basename(article["source"]))
+
+        if not os.path.exists("output"):
+            os.mkdir("output")
+        template_stream.dump("output/{}.html".format(filename))
