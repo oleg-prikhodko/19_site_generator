5b81399a48f518c21d06c1103744a303d867d4bb
Oleg <super.oleg.3000@yandex.ru>
2018-11-06 11:52:34 +0300

    Add topic title to article

--- main.py
+++ main.py
@@ -14,26 +14,27 @@ def load_text_from_file(filepath):
 if __name__ == "__main__":
     json_config = json.loads(load_text_from_file("config.json"))
     articles = json_config["articles"]
+    topics = {topic["slug"]: topic["title"] for topic in json_config["topics"]}
 
     for article in articles:
         md_path = "articles/{}".format(article["source"])
-        article_name = article["title"]
         md_content = load_text_from_file(md_path)
         html_content = markdown2.markdown(md_content)
+        filename, _ = os.path.splitext(os.path.basename(article["source"]))
+
+        article["url"] = "{}.html".format(filename)
+        article["topic"] = topics.get(article["topic"])
 
         environment = Environment(loader=FileSystemLoader("templates"))
         article_template = environment.get_template("article.html")
         article_template_stream = article_template.stream(
-            name=article_name, content=html_content
+            name=article["title"], topic=article["topic"], content=html_content
         )
-        filename, _ = os.path.splitext(os.path.basename(article["source"]))
 
         if not os.path.exists("output"):
             os.mkdir("output")
         article_template_stream.dump("output/{}.html".format(filename))
 
-        article["url"] = "{}.html".format(filename)
-
-    index_template = environment.get_template('index.html')
+    index_template = environment.get_template("index.html")
     index_template_stream = index_template.stream(articles=articles)
     index_template_stream.dump("output/index.html")
